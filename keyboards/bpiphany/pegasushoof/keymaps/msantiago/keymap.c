56aa157727ee91b97217384337ad9c46458717e2
Matthew Santiago <mattsantiago@gmail.com>
2021-01-21 14:53:23 -0800

    Added additional lock key

--- keyboards/bpiphany/pegasushoof/keymaps/msantiago/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/msantiago/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [KM_MEDIA] = LAYOUT( \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_WAKE, KC_PWR,  KC_SLEP, \
     KC_ENT,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,    _______, _______, KC_VOLU, \
-    KC_LOCK, KC_INS,  KC_HOME, KC_PGUP, KC_WBAK, KC_WFWD, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______,    _______, KC_MUTE, KC_VOLD, \
-    _______, KC_DEL,  KC_END,  KC_PGDN, KC_F5,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,                               \
+    KC_LOCK, KC_INS,  KC_HOME, KC_PGUP, KC_LOCK, KC_WFWD, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______,    _______, KC_MUTE, KC_VOLD, \
+    _______, KC_DEL,  KC_END,  KC_PGDN, KC_F5,   KC_WBAK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,                               \
     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______,          _______,             KC_MPLY,          \
     _______, _______, _______,                            KC_ESC,                             _______, _______, RESET,   _______,    KC_MPRV, KC_MSTP, KC_MNXT  \
   )
