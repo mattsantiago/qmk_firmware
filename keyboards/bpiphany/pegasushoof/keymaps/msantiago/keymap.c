4ac7f730ffbd72e6b566ace7344776e535353a0d
Matthew Santiago <mattsantiago@gmail.com>
2021-01-22 17:14:27 -0800

    Fixed layout issue for Pegasus Hoof

--- keyboards/bpiphany/pegasushoof/keymaps/msantiago/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/msantiago/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN, \
     FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                \
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,            \
+    KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,            \
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, FN_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
   /* Layer 1: Function layer */
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ENT,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TG(2),      _______, _______, KC_VOLU, \
     KC_LEAD, KC_INS,  KC_HOME, KC_PGUP, KC_LOCK, KC_WFWD, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______,    _______, KC_MUTE, KC_VOLD, \
     _______, KC_DEL,  KC_END,  KC_PGDN, KC_F5,   KC_WBAK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,                               \
-    _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______,          _______,             KC_MPLY,          \
+    _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______,          _______,             KC_MPLY,          \
     _______, _______, _______,                            KC_ESC,                             _______, _______, KC_APP,  _______,    KC_MPRV, KC_MSTP, KC_MNXT  \
   ),
   /* Layer 2: Mouse keys */
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(2),      _______, _______, _______, \
     _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, \
     TG(2),   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, _______, _______, _______, _______, _______,          _______,                               \
-    _______, KC_WH_L, KC_BTN3, KC_WH_R, _______, _______, _______, _______, _______, _______, _______, _______,          _______,             _______,          \
+    _______, _______, KC_WH_L, KC_BTN3, KC_WH_R, _______, _______, _______, _______, _______, _______, _______,          _______,             _______,          \
     _______, _______, _______,                            KC_ESC,                             _______, _______, _______, _______,    _______, _______, _______  \
   )
 
