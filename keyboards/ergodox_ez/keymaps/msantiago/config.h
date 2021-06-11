ff70ee79e792fdc5299f7adc40c660b3fb83c881
Matthew Santiago <mattsantiago@gmail.com>
2021-06-10 21:55:29 -0700

    Refactor Ergodox EZ configuration to move changes to keymap folder

---
+++ keyboards/ergodox_ez/keymaps/msantiago/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_MSANTIAGO_H
+#define CONFIG_MSANTIAGO_H
+
+#include "../../config.h"
+
+// #define ONESHOT_TAP_TOGGLE 2
+// #define ONESHOT_TIMEOUT 1000
+
+// #undef LEADER_TIMEOUT
+// #define LEADER_TIMEOUT 1000
+
+// #undef TAPPING_TERM
+// #define TAPPING_TERM 200
+
+// #undef TAPPING_TOGGLE
+// #define TAPPING_TOGGLE 2
+
+
+/* Leader key settings */
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 500
+//#define TAPPING_TERM 150
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+#endif
