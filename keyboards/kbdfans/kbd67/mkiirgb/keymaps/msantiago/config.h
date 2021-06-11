1b533fd0de7575e47da620da6e39c83070e88d17
Matthew Santiago <mattsantiago@gmail.com>
2021-06-11 00:01:41 -0700

    Update KBD67 msantiago keymap to include alt + space combo for number and symbol layer

---
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/msantiago/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_MSANTIAGO_H
+#define CONFIG_MSANTIAGO_H
+
+/* Leader key settings */
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 500
+#define TAPPING_TERM 150
+
+#define COMBO_COUNT 1
+
+#endif
